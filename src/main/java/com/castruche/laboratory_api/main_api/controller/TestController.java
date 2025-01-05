483fbe7c128ad3d4b96eb89f33445e3938ee77be
Castruche <aurelien.blot@gmail.com>
2025-01-05 14:50:59 +0100

    gestion connexion serveur

--- src/main/java/com/castruche/laboratory_api/main_api/controller/TestController.java
+++ src/main/java/com/castruche/laboratory_api/main_api/controller/TestController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping()
-    public String connexionTest() {
-        return "Test OK";
+    public boolean connexionTest() {
+        return true;
     }
 
 
