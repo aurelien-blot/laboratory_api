52929d837cff50ee6a10be63df0506d1cdb62bb3
Castruche <aurelien.blot@gmail.com>
2025-08-24 12:16:12 -0400

    Affinage dev prod

--- src/main/java/com/castruche/laboratory_api/LaboratoryApiApplication.java
+++ src/main/java/com/castruche/laboratory_api/LaboratoryApiApplication.java
@@ -2,12 +2,18 @@ package com.castruche.laboratory_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LaboratoryApiApplication {
+public class LaboratoryApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(LaboratoryApiApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LaboratoryApiApplication.class);
+    }
 }
